9eee68b20ee4e1c382f5e4325f3c9d4b3652645b
Richard Aber <richaber@gmail.com>
2022-04-22 16:13:04 -0500

    Add uninstall file

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Uninstall file.
+ *
+ * @link https://developer.wordpress.org/plugins/plugin-basics/uninstall-methods/
+ *
+ * @package RichAber\PluginBoilerplate
+ * @since   0.1.0-dev
+ */
+
+// If uninstall was not called by WordPress, exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
