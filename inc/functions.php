5f6d5f9cac3a7ff9215714cb6dd20e7ba68d3bc8
Richard Aber <richaber@gmail.com>
2022-04-22 17:23:54 -0500

    Add plugin functions file

---
+++ inc/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin functions file.
+ *
+ * This is intended to be used for "critical" functions used in the plugin bootstrap.
+ *
+ * @package RichAber\PluginBoilerplate
+ * @since   0.1.0-dev
+ */
+
+namespace RichAber\PluginBoilerplate;
