0abf7fb0d34a754379ae89b872739353f8c354e1
Richard Aber <richaber@gmail.com>
2022-04-25 14:16:29 -0500

    Add a config file for the PHP-DI container

---
+++ inc/Config/Container.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PHP-DI definitions configuration file.
+ *
+ * If this gets too complex,
+ * it could be split into multiple config files,
+ * then read and merge them all together here.
+ *
+ * @link    http://php-di.org/doc/php-definitions.html
+ *
+ * @since   0.1.0-dev
+ * @package RichAber\PluginBoilerplate
+ */
+
+namespace RichAber\PluginBoilerplate;
+
+return [];
