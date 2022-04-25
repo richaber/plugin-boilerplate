cedf96fbafb01db783350a530d472129bd595d2a
Richard Aber <richaber@gmail.com>
2022-04-25 13:57:30 -0500

    Add function to load generated classmaps

--- inc/functions.php
+++ inc/functions.php
@@ -30,3 +30,30 @@ function load_textdomain(): void {
 		) . '/languages/'
 	);
 }
+
+/**
+ * Load inc and dependency classmaps.
+ *
+ * @since 0.1.0-dev
+ *
+ * @return void
+ */
+function autoload_classes(): void {
+	$class_map = array_merge(
+		include PLUGINBOILERPLATE_DIR . 'inc/vendor/composer/autoload_classmap.php',
+		include PLUGINBOILERPLATE_DIR . 'dependencies/vendor/composer/autoload_classmap.php'
+	);
+
+	spl_autoload_register(
+		function ( $class ) use ( $class_map ) {
+			if (
+				isset( $class_map[ $class ] )
+				&& __NAMESPACE__ === substr( $class, 0, mb_strlen( __NAMESPACE__, 'UTF-8' ) )
+			) {
+				require_once $class_map[ $class ];
+			}
+		},
+		true,
+		true
+	);
+}
